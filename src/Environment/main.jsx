a70912e61380024d272df3f242954c61721ca0e4
Toby <109319639+CyberWolf101@users.noreply.github.com>
2023-10-17 01:16:45 -0700

    Add files via upload

---
+++ src/Environment/main.jsx
@@ -0,0 +1,25 @@
+import { Canvas } from "@react-three/fiber"
+import ReactDOM from "react-dom/client"
+import Experience from "./Experience"
+import { Loader } from "@react-three/drei"
+import Sidepage from "./libs/Sidepage"
+import { userStore } from "./store/store"
+
+ function ExperiencePage() {
+  <div>
+    <Canvas
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 100,
+        position: [0, 0, 1],
+      }}
+    >
+      <Experience />
+    </Canvas>
+    {/* page sidebar */}
+    <Sidepage />
+    <Loader />
+  </div>
+}
+export default ExperiencePage
