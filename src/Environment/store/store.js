41a071554c393ea3a1e68a0ee5490a02877823d0
Toby <109319639+CyberWolf101@users.noreply.github.com>
2023-10-25 03:43:54 -0700

    Delete src directory

--- src/Environment/store/store.js
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand"
-
-export const userStore = create()((set) => ({
-  open: false,
-  setOpen: (open) => set({ open: !open }),
-  setClose: (open) => set({ open: false }),
-}))
